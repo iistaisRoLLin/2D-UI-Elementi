bbd0f94efcb6acf07de713878d8317765c707eb0
Kristers <kristers.cernisevs@gmail.com>
2022-02-16 14:14:54 +0200

    Istenotas darbibas ar tekstu

---
+++ Assets/Skripti/TekstaParadisanaa.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TekstaParadisanaa : MonoBehaviour {
+	// Uzglaba tekstu kurs panemts no input lauka
+	public string teksts;		
+	// Lauks no kura es tekstu paņemšu
+	public GameObject ievadesLauks;
+	//Teksta objekts kur attēlot mainīga teksts saturu
+	public GameObject tekstaAttelosanasLauks;
+
+	public void darbibasArTekstu(){
+		teksts = ievadesLauks.GetComponent<Text>().text;
+		tekstaAttelosanasLauks.GetComponent<Text>().text = "Sveiks " + teksts.ToUpper () +"! ";
+
+	}
+
+	}
